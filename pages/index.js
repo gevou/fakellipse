ec31ad53bf48cfaf493617003be7ac28a907c2bf
George Voulgaris <george@voulgaris.net>
2020-06-25 05:23:08 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ const Home = () => (
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
-      <script dangerouslySetInnerHTML={{ __html: `
-window.Intellipse=window.Intellipse||function(){(Intellipse.q=Intellipse.q||[]).push(arguments)};Intellipse.l=+new Date; </script> <script async src='https://bootstrap.development.intellipse.com/bootstrap.js'>
-`}} />
+      <script dangerouslySetInnerHTML={{ __html: 
+  `window.Intellipse=window.Intellipse||function(){(Intellipse.q=Intellipse.q||[]).push(arguments)};Intellipse.l=+new Date; </script> <script async src='https://bootstrap.development.intellipse.com/bootstrap.js'>`
+  }} />
       </Head>
 
     <main>
