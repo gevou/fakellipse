98a92612bd240602c26cb7dbdc33bde503448ccd
George Voulgaris <george@voulgaris.net>
2020-06-25 05:17:52 +0300

    dev for easy testing

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = () => (
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
       <script dangerouslySetInnerHTML={{ __html: `
-window.Intellipse=window.Intellipse||function(){(Intellipse.q=Intellipse.q||[]).push(arguments)};Intellipse.l=+new Date; </script> <script async src='https://bootstrap.production.intellipse.com/bootstrap.js'>
+window.Intellipse=window.Intellipse||function(){(Intellipse.q=Intellipse.q||[]).push(arguments)};Intellipse.l=+new Date; </script> <script async src='https://bootstrap.development.intellipse.com/bootstrap.js'>
 `}} />
       </Head>
 
